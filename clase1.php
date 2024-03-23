432ae9c7c37c76ff56a247bc6a741be66a6dff91
Luis Enrique <luisenrique_031099@outlook.es>
2024-03-22 21:25:13 -0600

    Update clase1.php

--- clase1.php
+++ clase1.php
@@ -1,11 +1,11 @@
 <html>
-<body>
-<form action="formpost.php" method="post">
-    Nombre: <input type="text" name="nombre"><br>
-    Email: <input type="text" name="email"><br>
-    <input type="submit" value="Enviar">
-</form>
-</body>
+    <body>
+        <form action="formpost.php" method="post">
+            Nombre: <input type="text" name="nombre"><br>
+            Email: <input type="text" name="email"><br>
+            <input type="submit" value="Enviar">
+        </form>
+    </body>
 </html>
 
 <html>
