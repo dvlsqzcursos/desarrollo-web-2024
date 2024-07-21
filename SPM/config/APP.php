a9b1b2dccf020242c6833e198192d1d62a25bb08
dvlsqz <danielvel1977@gmail.com>
2024-07-20 21:12:43 -0600

    c

---
+++ SPM/config/APP.php
@@ -0,0 +1,19 @@
+<?php
+
+	/*----------  Ruta o dominio del servidor  ----------*/
+	const SERVERURL="http://localhost/SPM/";
+
+	/*----------  Nombre de la empresa o compañia  ----------*/
+	const COMPANY="SISTEMA PRESTAMOS ODL";
+
+	/*----------  Simbolo de la moneda  ----------*/
+	const MONEDA="Q";
+
+	/*----------  Zona horaria  ----------*/
+	date_default_timezone_set ("America/El_Salvador");
+
+	/*
+		Configuración de zona horaria de tu país para más información visita
+		http://php.net/manual/es/function.date-default-timezone-set.php
+		http://php.net/manual/es/timezones.php
+	*/
